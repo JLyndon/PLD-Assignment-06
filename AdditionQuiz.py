bec66dee42105a9052eb2cae2fa17df9305ec160
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-21 12:24:41 +0800

    Commit - Lines that Generates 2 Random Numbers

---
+++ AdditionQuiz.py
@@ -0,0 +1,13 @@
+import random
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+randNum_01 = random.randint(0,99)
+randNum_02 = random.randint(0,99)
+
+randompick = randNum_01, randNum_02
+
+print(randompick)
