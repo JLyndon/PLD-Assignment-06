fba62bbcf95f7c886a8bb8f4af9ca779a9689f70
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-23 02:55:04 +0800

    Defined Instructions for Questions, Added Opening Statement

--- AdditionQuiz.py
+++ AdditionQuiz.py
@@ -7,8 +7,6 @@ import random
 # The program will ask the user 10 addition operation
 # Display the result summary of the 10 operations (ex 9/10)
 
-QuestStatement = ("How about ", "Try ", "Add ", "Hmm. ", "This one: ", "Solve ", "Compute for ")
-
 def RandNumPickerStarter():
     randNum_01 = random.randint(0,99)
     randNum_02 = random.randint(0,99)
@@ -23,6 +21,7 @@ def RandNumPickerStarter():
         return "wrong"
 
 def RandNumPickerMid():
+    QuestStatement = ("How about ", "Try ", "What? Try this instead  ", "Hmm. ", "This one: ", "Solve ", "Compute for ")
     randNum_01 = random.randint(0,99)
     randNum_02 = random.randint(0,99)
     print(f"\n\n{random.choice(QuestStatement)}{randNum_01} + {randNum_02} = ____ ?")
@@ -35,21 +34,27 @@ def RandNumPickerMid():
     else:
         return "wrong"
 
-Usr_Score = 0
-Item_Num = 0
-while Item_Num < 10:
-    Item_Num += 1
-    if Item_Num == 1:
-        Question = RandNumPickerStarter()
-        if Question == "correct":
-            Usr_Score += 1
-        else:
-            None
-    elif Item_Num > 1:
-        Question_ = RandNumPickerMid()
-        if Question_ == "correct":
-            Usr_Score += 1
-        else:
-            None
+def QuizQuest():
+    Usr_Score = 0
+    Item_Num = 0
+    while Item_Num < 10:
+        Item_Num += 1
+        if Item_Num == 1:
+            Question = RandNumPickerStarter()
+            if Question == "correct":
+                Usr_Score += 1
+            else:
+                None
+        elif Item_Num > 1:
+            Question_ = RandNumPickerMid()
+            if Question_ == "correct":
+                Usr_Score += 1
+            else:
+                None
+    print(f"\nYour score: {Usr_Score}/10")
 
-print(f"\nYour score: {Usr_Score}/10")
+print("\n                Welcome to Add-trocious Monster's Quiz!\n Do you dare to take the TEST? Finish with a high score to get acknowledged.\n\nType '\33[92mstart\33[0m' to get started.\nType '\33[91mexit\33[0m' to close the program.")
+
+
+
+QuizQuest()
