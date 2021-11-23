e970577fbd983a23167307fbc871dfe0f1ee3e04
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-23 13:19:02 +0800

    Adjusted Conditions for AnswerChckr, Revised Statements for QuizQuest, and MainProg

--- AdditionQuiz.py
+++ AdditionQuiz.py
@@ -11,15 +11,15 @@ import time
 def RandNumPickerBegin(Set_Item, Difficulty):
     if Set_Item == 0:
         randNum_01, randNum_02 = Randomize(Difficulty)
-        print(f"\n\nWhat is {randNum_01} + {randNum_02} = ____ ?")
-        Usr_Answer_0 = input("\nYour answer, here.\n\n> ")
+        print(f"\n\nWhat is \33[92m{randNum_01}\33[0m + \33[92m{randNum_02}\33[0m = ____ ?")
+        Usr_Answer_0 = input("\n\33[1m\33[3m\33[94mYour answer: \33[0m")
         Item_Result_0 = CheckingAns(str(Usr_Answer_0), str(randNum_01), str(randNum_02))
         return Item_Result_0
     elif Set_Item == 1:
-        QuestStatement = ["How about ", "Try ", "What? Try this instead  ", "Hmm. ", "This one: ", "Solve ", "Compute for "]
+        QuestStatement = ["How about ", "Try ", "What? Try this instead ", "Hmm. ", "This one: ", "Solve ", "Compute for "]
         randNum_11, randNum_22 = Randomize(Difficulty)
-        print(f"\n\n{random.choice(QuestStatement)}{randNum_11} + {randNum_22} = ____ ?")
-        Usr_Answer_1 = input("\nYour answer, here.\n\n> ")
+        print(f"\n\n{random.choice(QuestStatement)}\33[92m{randNum_11}\33[0m + \33[92m{randNum_22}\33[0m = ____ ?")
+        Usr_Answer_1 = input("\n\33[1m\33[3m\33[94mYour answer: \33[0m")
         Item_Result_1 = CheckingAns(str(Usr_Answer_1), str(randNum_11), str(randNum_22))
         return Item_Result_1
 
@@ -42,27 +42,29 @@ def Randomize(Set_Difficulty):
         return rand_11, rand_22
 
 def CheckingAns(Answer, FirstRandom, SecondRandom):
-    if "." in FirstRandom and SecondRandom:
-        if Answer.replace(".","", 1).isdecimal() == True:
-            if float(Answer) == (float(FirstRandom) + float(SecondRandom)):
-                return "correct"
-            else:
-                return "wrong"
-        else:
-            return "wrong"
-    elif "." not in FirstRandom and SecondRandom:
-        if Answer.isdecimal() == True:
-            if int(Answer) == (int(FirstRandom) + int(SecondRandom)):
-                return "correct"
-            else:
-                return "wrong"
-        else:
-            return "wrong"
+    if (Answer == None) or (Answer == "") or (Answer.isspace() == True):
+        return "wrong", "noinput"
     else:
-        if (Answer == None) or (Answer == "") or (Answer.isspace() == True):
-            return "wrong"
+        if "." in FirstRandom and SecondRandom:
+            sum_0 = round(float(FirstRandom) + float(SecondRandom),4)
+            if Answer.replace(".","", 1).isdecimal() == True:
+                if float(Answer) == sum_0:
+                    return "correct", sum_0
+                else:
+                    return "wrong", sum_0
+            else:
+                return "wrong", "invalid"
+        elif "." not in FirstRandom and SecondRandom:
+            sum_1 = (int(FirstRandom) + int(SecondRandom))
+            if Answer.isdecimal() == True:
+                if int(Answer) == sum_1:
+                    return "correct", sum_1
+                else:
+                    return "wrong", sum_1
+            else:
+                return "wrong", "invalid"
         else:
-            return "wrong"
+            return "wrong", "invalid"
 
 def QuizQuest(Chosen_Difficulty):
     Usr_Score = 0
@@ -70,44 +72,56 @@ def QuizQuest(Chosen_Difficulty):
     while Item_Num < 10:
         Item_Num += 1
         if Item_Num == 1:
-            Question = RandNumPickerBegin(0, Chosen_Difficulty)
+            Question, Total = RandNumPickerBegin(0, Chosen_Difficulty)
             if Question == "correct":
+                print("\n\33[92mCorrect!\33[0m")
                 Usr_Score += 1
-            else:
-                print("Incorrect!")
-                None
+            elif Question == "wrong":
+                if Total == "noinput":
+                    print("\33[91mIncorrect! You have no input!\33[0m")
+                elif Total == "invalid":
+                    print("\n\33[91mIncorrect! Invalid answer format.\33[0m")
+                else:
+                    print(f"\n\33[91mIncorrect!\33[0m The correct answer is \33[93m{Total}\33[0m")
         elif Item_Num > 1:
-            Question_ = RandNumPickerBegin(1, Chosen_Difficulty)
+            Question_, Total_ = RandNumPickerBegin(1, Chosen_Difficulty)
             if Question_ == "correct":
+                print("\n\33[92mCorrect!\33[0m")
                 Usr_Score += 1
-            else:
-                print("Incorrect!")
-                None
+            elif Question_ == "wrong":
+                if Total_ == "noinput":
+                    print("\33[91mIncorrect! You have no input!\33[0m")
+                elif Total_ == "invalid":
+                    print("\n\33[91mIncorrect! Invalid answer format.\33[0m")
+                else:
+                    print(f"\n\33[91mIncorrect!\33[0m The correct answer is \33[93m{Total_}\33[0m")
+
     print(f"\nYou got {Usr_Score} out of 10 equations right.")
 
 def DifficultySelection(DifficOfChoice):
     if (DifficOfChoice == "normal") or (DifficOfChoice == "1"):
-        print("\n\n                    \33[100m NORMAL DIFFICULTY \33[0m")
-        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n".center(60, " "))
+        print("\n\n\n                    \33[100m NORMAL DIFFICULTY \33[0m")
+        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n".center(60, " "))
         return QuizQuest('normal')
     elif (DifficOfChoice == "veteran") or (DifficOfChoice == "2"):
-        print("\n\n                  \33[44m VETERAN DIFFICULTY \33[0m")
-        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n".center(60, " "))
+        print("\n\n\n                  \33[44m VETERAN DIFFICULTY \33[0m")
+        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n".center(60, " "))
         return QuizQuest('veteran')
     elif (DifficOfChoice == "intermediate") or (DifficOfChoice == "3"):
-        print("\n\n               \33[41m INTERMEDIATE DIFFICULTY \33[0m")
-        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n".center(60, " "))
+        print("\n\n\n               \33[41m INTERMEDIATE DIFFICULTY \33[0m")
+        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n".center(60, " "))
         return QuizQuest('intermediate')
     else:
-        return print("\33[91mUnknown Difficulty\33[0m")
+        print("\33[91mUnknown Difficulty\33[0m")
+        return "notvalid"
 
 def DifficultyComment(RandomDiffic):
     if RandomDiffic == "normal":
-        return print("\n\n\33[92mLucky you!\33[0m Answer in:")
+        return print("\n\n\n\33[92mLucky you!\33[0m Answer in:")
     elif RandomDiffic == "veteran":
-        return print("\n\n\33[94mGood luck!\33[0m Answer in:")
+        return print("\n\n\n\33[94mGood luck\33[0m answering in:")
     elif RandomDiffic == "intermediate":
-        return print("\n\n\33[91mOho. \33[0mBrace yourself as you will answer in:")
+        return print("\n\n\n\33[91mOho. \33[0mBrace yourself as you will answer in:")
 
 def Decor_MonsterFace():
     output = ""
@@ -129,17 +143,22 @@ print("\33[92mYes\33[0m, \33[91mNo\33[0m or.. \33[94m\33[1mRUN?!\33[0m".center(8
 while True:
     Usr_Decision = input("\n> ").lower()
     if Usr_Decision.replace("!","").replace("?","") == "yes":
-        print("\n\n\33[3mVery well.. \33[0m\n\nChoose your difficulty:\nType '\33[92m1\33[0m' or '\33[92mnormal\33[0m' for normal difficulty.\nType '\33[94m2\33[0m' or '\33[94mveteran\33[0m' for veteran difficulty.\nType '\33[91m3\33[0m' or '\33[91mintermediate\33[0m' for intermediate difficulty.")
+        print("\n\n\33[3m\33[92mVery well.. \33[0m")
+        time.sleep(1.7)
+        print("\n\nChoose your difficulty:\nType '\33[92m1\33[0m' or '\33[92mnormal\33[0m' for normal difficulty.\nType '\33[94m2\33[0m' or '\33[94mveteran\33[0m' for veteran difficulty.\nType '\33[91m3\33[0m' or '\33[91mintermediate\33[0m' for intermediate difficulty.")
         while True:
             ChoosingDiffi = input("\n\n> ").lower()
-            DifficultySelection(ChoosingDiffi)
-            break
+            IsInptValid = DifficultySelection(ChoosingDiffi)
+            if IsInptValid == "notvalid":
+                pass
+            else:
+                break
         break
     elif Usr_Decision.replace("!","").replace("?","") == "run":
-        print("\n\n\33[3m\33[91mOh no you wont..\33[0m\33[3m\n\nThere's no escape! You have to give me an answer!\33[0m")
-        time.sleep(1.5)
-        print("\nAs punishment, I'll \33[91m\33[1mrandomly\33[0m choose the difficulty of your trial.")
-        time.sleep(1.3)
+        print("\n\n\33[3m\33[91mOh no you won't..\33[0m\33[3m\n\nThere's no escape! You have to give me an answer!\33[0m")
+        time.sleep(1.7)
+        print("\nAs punishment for running away, I'll \33[91m\33[1mrandomly\33[0m select the difficulty of your trial.\n   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        time.sleep(1.9)
         RandomDifficulty = random.choice(["veteran", "normal", "intermediate"])
         DifficultyComment(RandomDifficulty)
         time.sleep(1.3)
